1c95b0b692bb3df7754c96c6b55e3a57add21676
xiong mao <502474264@qq.com>
2023-12-20 14:29:02 +0800

    min fix

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -2,14 +2,13 @@ package com.demo;
 
 import com.demo.khh.Stream;
 import java.io.*;
-// import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.DateUtil;
 
 public class Main {
 
     public static void main(String[] args) throws IOException  {
 
-        // System.out.println(DateUtil.today());
-
+        System.out.println(DateUtil.today());
         Stream.bRead();
     }
 }
